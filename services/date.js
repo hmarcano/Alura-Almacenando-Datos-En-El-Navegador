9c2b8dca1d5df6ca174dd4d6c6723d6f004d9804
Harold <harold_marcano@hotmail.com>
2022-12-05 15:00:14 -0300

    Clase Final Proyecto Completo

---
+++ services/date.js
@@ -0,0 +1,18 @@
+export const uniqueDates = (tasks) => {
+  const unique = [];
+
+  tasks.forEach((task) => {
+    if (!unique.includes(task.dateFormat)) unique.push(task.dateFormat);
+  });
+
+  return unique;
+};
+
+export const orderDates = (dates) => {
+  return dates.sort( (a, b) => {
+    const firstDate = moment(a, "DD/MM/YYY");
+    const secondtDate = moment(b, "DD/MM/YYY");
+
+    return firstDate - secondtDate;
+  });
+};
